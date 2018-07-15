2bddd6c25468f99b0f205020c793493fa79a2f77
jinhao11 <jinhao11@163.com>
2018-07-15 22:23:16 +0800

    [mod]使用wait和notifyAll实现超时获取线程池的例子

--- src/main/java/com/halle/java/base/proxy/cglib/CglibProxyTest.java
+++ src/main/java/com/halle/java/base/proxy/cglib/CglibProxyTest.java
@@ -1,4 +1,13 @@
 package com.halle.java.base.proxy.cglib;
 
 public class CglibProxyTest {
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            public void run() {
+                while (true){
+                    System.out.println(1);
+                }
+            }
+        }).start();
+    }
 }
